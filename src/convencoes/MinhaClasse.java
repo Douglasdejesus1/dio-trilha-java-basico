01e3eaf2d0016b8b3740551c88ef595d7620fd89
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-02-27 19:35:29 -0300

    anatomia das classes

---
+++ src/convencoes/MinhaClasse.java
@@ -0,0 +1,21 @@
+package convencoes;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+		int _um = 10;
+
+		_um = 15;
+		System.out.println(_um);
+		System.out.println(somar(3, 5));
+
+	}
+
+	public static int somar(int a, int b) {
+
+		return a + b;
+	}
+
+}
