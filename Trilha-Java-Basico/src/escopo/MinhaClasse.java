3be54a570ad66150f5eb30a97816f450ce1e9a09
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-03-01 21:25:51 -0300

    Escopo

---
+++ Trilha-Java-Basico/src/escopo/MinhaClasse.java
@@ -0,0 +1,17 @@
+package escopo;
+
+public class MinhaClasse {
+	public static void main(String[] args) {
+		String carro = "Gol";
+		System.out.println(carro);
+		imprimirCarro();
+		
+		
+		
+	}
+	public static void imprimirCarro() {
+		String carro ="Ferrari";
+		System.out.println(carro);
+	}
+
+}
