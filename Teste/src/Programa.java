a0a04ba34a4ba2aa88387343285781e7ec36cb16
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-03-06 22:18:36 -0300

    debbuging

---
+++ Teste/src/Programa.java
@@ -0,0 +1,20 @@
+public class Programa {
+	public static void main(String[] args) {
+
+		//Scanner in = new Scanner(System.in);
+		int t = 2;
+		for (int y = 0; y < t; y++) {
+			int a = 0;
+			int b = 2;
+			int n = 10;
+			int soma = 0;
+			for (int i = 0; i < n; i++) {
+				System.out.print(i);
+			}
+			System.out.println();
+
+			
+		}
+
+	}
+}
